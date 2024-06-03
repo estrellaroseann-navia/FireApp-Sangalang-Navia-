cf8a81d03ed5d91f351f63e007ecbed5c63e1c0b
estrellaroseann_navia <202180026@psu.palawan.edu.ph>
2024-06-03 22:24:18 +0800

    dynamic pie chart

--- projectsite/fire/views.py
+++ projectsite/fire/views.py
@@ -1,11 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from fire.models import Locations, Incident
-from django.db import connection
-from django.http import JsonResponse
-from django.db.models.functions import ExtractMonth
-from django.db.models import Count
-from datetime import datetime
 
 from django.db import connection
 from django.http import JsonResponse
